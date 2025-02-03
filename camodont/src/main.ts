795bde2ba339eacfdf7632d3975bf8b252f8488e
DavidIntriago <david.intriago@unl.edu.ec>
2025-02-03 09:21:06 -0500

    Cors

--- camodont/src/main.ts
+++ camodont/src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
 
   
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Permite cualquier origen
+  });
+  
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 4000);
 
