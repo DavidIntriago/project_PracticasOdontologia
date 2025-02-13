32e0620b05de480042029ced59387f446372edb5
DavidIntriago <david.intriago@unl.edu.ec>
2025-02-12 23:36:09 -0500

    actualizacion login

--- camodont/src/modules/auth/auth.service.ts
+++ camodont/src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login';
 import { PrismaService } from 'src/db/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,10 +15,14 @@ constructor (private prisma: PrismaService) {}
       },
     });
     console.log(usuario);
+    const salt = parseInt(process.env.CODE_BCRYPT_SALT || '10');
+    const isMatch = await bcrypt.compare(login.clave, usuario.clave);
+    console.log(isMatch);
+
     if (!usuario) {
       return { error: 'Usuario no encontrado' };
     }
-    if (usuario.clave != login.clave) {
+    if (!isMatch) {
       return { error: 'Clave incorrecta' };
     }else{
     return usuario;
