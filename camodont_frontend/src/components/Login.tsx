78ac952a59aa15e4147afb995e9d0c2b4971f29c
DavidIntriago <david.intriago@unl.edu.ec>
2025-03-08 23:16:48 -0500

    update Login component to save external_id

--- camodont_frontend/src/components/Login.tsx
+++ camodont_frontend/src/components/Login.tsx
@@ -46,7 +46,7 @@ const Login = ({isOpen, onClose}: LoginProps) => {
         alert("Bienvenid@ "+response.nombre);
 
         save("rol", response.idRol);
-        save("id", response.external_id);
+        save("external_id", response.external_id);
         router.push("/indexUsers"); 
 
         return;
